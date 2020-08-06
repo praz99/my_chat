826d1e7acc0890e7ed888a31e93120d87d91b7ca
praz <t.prazwal@gmail.com>
2020-08-06 15:11:57 +0200

    testing JS linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ["eslint-config-airbnb-base",
+"plugin:prettier/recommended"],
+
+  env: {
+    browser: true
+  },
+
+  parser: "babel-eslint",
+
+  setting: {
+    "import/resolver": {
+      webpack: {
+        config: {
+          resolve: {
+            modules: ["frontend", "node_modules"]
+          }
+        }
+      }
+    }
+  }
+};
