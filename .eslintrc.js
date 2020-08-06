8dcf37ab2d9e6e7fbd02b3684e2fa0a0378d3b9a
praz <t.prazwal@gmail.com>
2020-08-06 16:14:40 +0200

    fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
-  extends: ["eslint-config-airbnb-base",
-"plugin:prettier/recommended"],
+  extends: ["eslint-config-airbnb-base", "plugin:prettier/recommended"],
 
   env: {
     browser: true
